711ba68f6298e4d525421a0864baec4c1cd2d606
Matthew Evans <git@ml-evs.science>
2022-09-25 09:06:54 +0000

    Cherry-pick new functionality in  module for creating new connections

--- pydatalab/pydatalab/mongo.py
+++ pydatalab/pydatalab/mongo.py
@@ -1,4 +1,6 @@
 from flask_pymongo import PyMongo
+from pymongo import MongoClient
+from pymongo.errors import ServerSelectionTimeoutError
 
 flask_mongo = PyMongo()
 
@@ -22,4 +24,28 @@ def check_mongo_connection():
     return True
 
 
-__all__ = ("flask_mongo", "check_mongo_connection")
+def _get_active_mongo_client(timeoutMS: int = 100) -> MongoClient:
+    """Returns a `MongoClient` for the configured `MONGO_URI`,
+    raising a `RuntimeError` if not available.
+    Parameters:
+        timeoutMS: Value to use for the MongoDB timeouts (connect and server select)
+            in milliseconds
+    Returns:
+        The active MongoClient, already connected.
+    """
+    from pydatalab.config import CONFIG
+    from pydatalab.logger import LOGGER
+
+    try:
+        return MongoClient(
+            CONFIG.MONGO_URI,
+            connectTimeoutMS=timeoutMS,
+            serverSelectionTimeoutMS=timeoutMS,
+            connect=True,
+        )
+    except ServerSelectionTimeoutError as exc:
+        LOGGER.critical(f"Unable to connect to MongoDB at {CONFIG.MONGO_URI}")
+        raise RuntimeError from exc
+
+
+__all__ = ("flask_mongo", "check_mongo_connection", "_get_active_mongo_client")
