d712f4721e1ba8ac2e3323a9883f1d1b530af94a
Benjamin CHARMES <benjamin.charmes@gmail.com>
2024-06-25 16:28:39 +0100

    Add FormattedItemName, ChemicalFormula and IsoDatetimeToDate

--- webapp/src/components/SampleTableAgGrid.vue
+++ webapp/src/components/SampleTableAgGrid.vue
@@ -25,12 +25,73 @@ import { AgGridVue } from "ag-grid-vue3";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
 import { getSampleList, deleteSample } from "@/server_fetch_utils.js";
+import FormattedItemName from "@/components/FormattedItemName";
+import ChemicalFormula from "@/components/ChemicalFormula";
+import CollectionList from "@/components/CollectionList";
+import Creators from "@/components/Creators";
+import { h, getCurrentInstance } from "vue";
+
+const FormattedItemNameAg = {
+  setup(props) {
+    const { params } = props;
+    return () =>
+      h(FormattedItemName, {
+        item_id: params.value,
+        "item-type": params.data.type,
+        "enable-modified-click": true,
+      });
+  },
+};
+
+const ChemicalFormulaAg = {
+  setup(props) {
+    const { params } = props;
+    return () => h(ChemicalFormula, { formula: params.value });
+  },
+};
+
+const IsoDatetimeToDateAg = {
+  setup(props) {
+    const instance = getCurrentInstance();
+    const { params } = props;
+    const filters = instance.appContext.config.globalProperties.$filters;
+
+    return () => h("span", filters.IsoDatetimeToDate(params.value));
+  },
+};
+
+const CollectionListAg = {
+  setup(props) {
+    const { params } = props;
+    return () => h(CollectionList, { collections: params.value });
+  },
+};
+
+const CreatorsAg = {
+  setup(props) {
+    const { params } = props;
+    return () =>
+      h(Creators, {
+        creators: params.value,
+      });
+  },
+};
 
 export default {
   components: {
     AgGridVue,
+    // eslint-disable-next-line
+    FormattedItemNameAg,
+    // eslint-disable-next-line
+    ChemicalFormulaAg,
+    // eslint-disable-next-line
+    IsoDatetimeToDateAg,
+    // eslint-disable-next-line
+    CollectionListAg,
+    // eslint-disable-next-line
+    CreatorsAg,
   },
-  data() {
+  setup() {
     return {
       isSampleFetchError: false,
       searchValue: "",
@@ -39,6 +100,7 @@ export default {
         {
           headerName: "ID",
           field: "item_id",
+          cellRenderer: "FormattedItemNameAg",
           checkboxSelection: true,
           headerCheckboxSelection: true,
           // floatingFilter: true,
@@ -46,21 +108,31 @@ export default {
         },
         { headerName: "Type", field: "type", flex: 1 },
         { headerName: "Sample name", field: "name", flex: 1 },
-        { headerName: "Formula", field: "chemform", flex: 1 },
+        { headerName: "Formula", field: "chemform", cellRenderer: "ChemicalFormulaAg", flex: 1 },
         {
           headerName: "Date",
           field: "date",
+          cellRenderer: "IsoDatetimeToDateAg",
           flex: 1,
-          valueFormatter: (params) => {
-            const date = new Date(params.value);
-            const year = date.getFullYear();
-            const month = String(date.getMonth() + 1).padStart(2, "0");
-            const day = String(date.getDate()).padStart(2, "0");
-            return `${year}-${month}-${day}`;
-          },
         },
-        { headerName: "Collection", field: "collection", flex: 1 },
-        { headerName: "Creators", field: "creators", flex: 1 },
+        {
+          headerName: "Collections",
+          field: "collections",
+          cellRenderer: "CollectionListAg",
+          valueFormatter: (params) => {
+            return Object.values(params.value);
+          },
+          flex: 1,
+        },
+        {
+          headerName: "Creators",
+          field: "creators",
+          cellRenderer: "CreatorsAg",
+          valueFormatter: (params) => {
+            return Object.values(params.value);
+          },
+          flex: 1,
+        },
         { headerName: "# of blocks", field: "nblocks", flex: 1 },
       ],
       rowSelection: "multiple",
